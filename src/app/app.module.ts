a612fba07800a7386f9542af86b243603d80589d
pradipshcpl@gmail.com <pradipshcpl@gmail.com>
2020-03-05 02:21:43 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RoutingModule} from './routing/routing.module';
+import { HttpModule } from '@angular/http';
+import { FormsModule} from '@angular/forms';
+
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+
+import { GithubService } from './github-services/github.service';
+
+import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { DateCountPipe } from './date-count.pipe';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubComponent,
+    DateCountPipe
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    HttpModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule,
+    FormsModule
+    
+  ],
+  providers: [GithubService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
